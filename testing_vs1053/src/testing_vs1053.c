fad4c149709a48368cd5a902c74926cbbf65b33b
marigisee <marigisee@gmail.com>
2025-10-06 16:34:50 -0300

    Initial commit

---
+++ testing_vs1053/src/testing_vs1053.c
@@ -0,0 +1,37 @@
+/*=============================================================================
+ * Program: testing_vs1053
+ * Date: 2025/10/06
+ * Version: 1
+ *===========================================================================*/
+
+/*=====[Inclusions of function dependencies]=================================*/
+
+#include "testing_vs1053.h"
+#include "sapi.h"
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Definitions of extern global variables]==============================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Main function, program entry point after power on or reset]==========*/
+
+int main( void )
+{
+   // ----- Setup -----------------------------------
+   boardInit();
+
+   // ----- Repeat for ever -------------------------
+   while( true ) {
+      gpioToggle(LED);
+      delay(500);
+   }
+
+   // YOU NEVER REACH HERE, because this program runs directly or on a
+   // microcontroller and is not called by any Operating System, as in the 
+   // case of a PC program.
+   return 0;
+}
