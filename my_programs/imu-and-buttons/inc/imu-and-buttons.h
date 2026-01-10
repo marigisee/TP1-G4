adfb14f373015b06b2e27a8739bd48c8791c574e
marina <marigisee@gmail.com>
2026-01-10 16:44:19 -0300

    imu and buttons program created

---
+++ my_programs/imu-and-buttons/inc/imu-and-buttons.h
@@ -0,0 +1,43 @@
+/*=============================================================================
+ * Copyright (c) 2026, Marina Cuello <marina.cuello@alu.ing.unlp.edu.ar>
+ * All rights reserved.
+ * License:  (see LICENSE.txt)
+ * Date: 2026/01/10
+ * Version: 0
+ *===========================================================================*/
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __IMU-AND-BUTTONS_H__
+#define __IMU-AND-BUTTONS_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*=====[C++ - begin]=========================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*=====[Definition macros of public constants]===============================*/
+
+/*=====[Public function-like macros]=========================================*/
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+/*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+/*=====[C++ - end]===========================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __IMU-AND-BUTTONS_H__ */
--- program.mk
+++ program.mk
@@ -2,14 +2,18 @@
 # This file select a program to compile with make
 #==============================================================================
 
-# ---------- Example of path -------------------------------------------
+# ---------- Example of path inside this file folder -------------------
 # Program path
 #(relative to this folder, leave void if the program is in this folder)
 #PROGRAM_PATH = examples_c/sapi/gpio
 # Program name
 #PROGRAM_NAME = blinky
 
+# ---------- Example of path outside this file folder ------------------
+#PROGRAM_PATH = ../examples
+#PROGRAM_NAME = blinky
+
 # ---------- Your paths: -----------------------------------------------
 
 PROGRAM_PATH = my_programs
-PROGRAM_NAME = test_latency_buttons_esp32_emisora
+PROGRAM_NAME = imu-and-buttons
